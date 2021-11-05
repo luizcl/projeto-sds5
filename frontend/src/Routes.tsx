d20db4e1b385febcf9dd4b792d64710dcee037b9
Luiz Claudio Moura Moraes <luiz@MacBook-Pro-de-Luiz.local>
2021-11-05 17:49:23 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,20 @@
+import Dashboard from "pages/Dashboard";
+import Home from "pages/Home";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <Home/>
+                </Route>
+                <Route path="/dashboard">
+                    <Dashboard/>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Router;
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Página Home</h1>
+            <a href='/dashboard'>Dashboard</a>
+        </div>
+    );
+}
+
+export default Home;
